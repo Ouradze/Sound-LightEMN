d9603858950677ef8ad25f9e82cb3be34986e691
Willcani <facchiniguillaume@gmail.com>
2014-03-11 11:17:28 +0100

    new file:   bin/design/Barre.class 	modified:   bin/design/Design.class 	new file:   bin/design/Rond.class 	new file:   bin/design/Stripe.class 	new file:   bin/processing/Barre.class 	modified:   bin/processing/MyProcessingSketch.class 	new file:   bin/sound/Fourier.class 	new file:   src/design/Barre.java 	modified:   src/design/Design.java 	new file:   src/design/Rond.java 	new file:   src/design/Stripe.java 	new file:   src/processing/Barre.java 	deleted:    bin/processing/Stripe.class

---
+++ src/design/Stripe.java
@@ -0,0 +1,64 @@
+package design;
+
+
+import processing.core.PApplet;
+import sound.Fourier;
+
+public class Stripe {
+
+	public float[] freq;
+	
+	public PApplet parent;
+	public float largeur;
+	public float espace;
+	public int sens;
+	public int positionX;
+	public int positionY;
+
+	public Stripe(PApplet p, int s, int x, int y) {
+		this.sens = s;
+		this.parent = p;
+		this.positionX = x;
+		this.positionY = y;
+
+	}
+
+	public void maj(float[] freq) {
+
+		
+			this.freq = Fourier.QuadScaling(Fourier.scaling(freq, 1.9, 0.1));
+			
+	}
+
+	public void display() {
+		
+
+		espace =(float) 0.5;
+		float[] freq2 = Fourier.CutScaling(this.freq, (float) 1.5);
+
+		largeur = (float)(parent.width / (2*freq.length))+espace;
+	
+			for (int i = 1; i < freq.length; i++) {
+
+				parent.pushMatrix();
+				parent.translate(positionX, positionY);
+
+				parent.fill(255, 0,0);
+				parent.stroke(0);
+				parent.rect(sens * i * (largeur + espace), -freq[i], largeur,
+						2 * freq[i] + 2);
+				
+				parent.fill(240, 0,0);
+				parent.stroke(0);
+				parent.rect(sens * i * (largeur + espace), -freq2[i], largeur,
+						2 * freq2[i] + 2);
+
+				parent.popMatrix();
+			}
+			
+		
+	}
+
+	
+
+}
