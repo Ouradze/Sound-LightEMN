3b0197457e9bebc1f091fcea994a9f7e0104d65b
Willcani <facchiniguillaume@gmail.com>
2014-03-25 00:21:27 +0100

    new file:   bin/java.policy.applet 	new file:   bin/sound/FileFreq.class 	new file:   src/sound/FileFreq.java

---
+++ src/sound/FileFreq.java
@@ -0,0 +1,37 @@
+package sound;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Queue;
+import java.util.Stack;
+
+public class FileFreq {
+	
+	private ArrayList<float[]> list;
+	private int n;
+	
+	public FileFreq(int n){
+		this.n=n;
+		list = new ArrayList<float[]>();
+	}
+	
+	
+	public void maj(float[] f){
+		list.add(0, f);
+		if(list.size()>n){
+			list.remove(list.size()-1);
+		}
+	}
+	
+	public float[] getIndex(int i){
+		return list.get(i);
+	}
+	
+	public int getsize(){
+		return list.size();
+	}
+	
+
+	
+}
