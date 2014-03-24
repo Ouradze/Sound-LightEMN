a35cdff2046d3264d7fd4f7a5986828f3c88a607
Willcani <facchiniguillaume@gmail.com>
2014-03-24 20:55:28 +0100

    new file:   bin/controller/ControlJButtonDefinedForm.class 	new file:   bin/controller/ControlJButtonFile.class 	new file:   bin/controller/ControlJButtonStream.class 	new file:   bin/model/MusicFile.class 	new file:   src/controller/ControlJButtonDefinedForm.java 	new file:   src/controller/ControlJButtonFile.java 	new file:   src/controller/ControlJButtonStream.java 	new file:   src/model/MusicFile.java 	new file:   src/processing/MyProcessingSketch.java 	Music/

---
+++ src/model/MusicFile.java
@@ -0,0 +1,13 @@
+package model;
+
+public class MusicFile {
+	private String path;
+	
+	public MusicFile(String path){
+		this.path = path;
+	}
+	
+	public String getPath(){
+		return this.path;
+	}
+}
