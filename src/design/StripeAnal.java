ebdd29a7c649daa667a42a0cb5b71cd2c2176bd9
Willcani <facchiniguillaume@gmail.com>
2014-04-29 11:48:50 +0200

    nouveau fichier: bin/design/StripeAnal.class 	nouveau fichier: bin/sound/AnalyseNote.class 	nouveau fichier: src/design/StripeAnal.java 	nouveau fichier: src/sound/AnalyseNote.java

---
+++ src/design/StripeAnal.java
@@ -0,0 +1,83 @@
+package design;
+
+
+import processing.core.PApplet;
+import sound.AnalyseNote;
+import sound.CalculFourier;
+import sound.Fourier;
+import sound.Lissage;
+
+public class StripeAnal extends Design{
+	
+	public float largeur;
+	public float espace;
+	public int sens;
+	AnalyseNote anal;
+	
+
+	public StripeAnal(PApplet p,  int x, int y) {
+		super(p,x,y, 0);
+		anal = new AnalyseNote();
+		this.coefs = new double[] { 0.2, 0.23, 0.23, 0.24, 0.24, 0.24, 0.24,
+				0.24, 0.24, 0.25, 0.25, 0.25, 0.25, 0.27, 0.28, 0.28, 0.28,
+				0.28, 0.28, 0.28, 0.28, 0.28, 0.28, 0.29, 0.31, 0.31, 0.33,
+				0.36, 0.38, 0.41, 0.41, 0.41, 0.41, 0.41, 0.44, 0.46, 0.5,
+				0.54, 0.59, 0.66, 0.71, 0.77, 0.82, 0.85, 0.9, 0.93, 0.96,
+				0.95, 0.98, 0.99, 0.99, 0.99, 0.98, 0.99, 0.99, 0.99, 0.99,
+				0.99, 0.99, 0.99, 0.99, 0.99, 0.99, 0.99, 0.99, 0.99, 0.99,
+				0.99, 0.99, 0.99, 0.99, 0.99, 0.99, 0.99, 0.99, 0.99, 0.99,
+				0.99, 0.99, 0.99, 0.99, 0.99, 0.99, 0.99, 0.99, 0.99, 0.99,
+				0.99, 0.99, 0.99 };
+	}
+
+	
+
+	public void display() {
+
+		espace = (float) 0.5;
+		
+
+		largeur = (float) (parent.getWidth() / (2 * this.freqcentre.length)) + espace;
+		parent.pushMatrix();
+		parent.translate(positionX, positionY,-100);
+		for (int i = 1; i < this.freqcentre.length; i++) {
+
+			
+
+			parent.fill(0);
+			parent.stroke(255);
+			parent.rect( i * (largeur + espace), -freqcentre[i], largeur, // cr�e un rectangle
+					2 * freqcentre[i] + 2);
+
+			parent.fill(255);
+			parent.stroke(255);
+			parent.rect(-i * (largeur + espace), -freqcentre[i], largeur,
+					2 * freqcentre[i] + 2);
+
+			
+		}
+		parent.popMatrix();
+
+	}
+
+	@Override
+	public void scale() {
+		//this.freqcentre = CalculFourier.CutScaling(Lissage.Lissercubique(this.freqcentre),(float) 0.8);
+		//this.freqgauche = CalculFourier.CutScaling(Lissage.Lissercubique(this.freqgauche),(float) 0.8);
+		//this.freqdroite =CalculFourier.CutScaling(Lissage.Lissercubique(this.freqdroite),(float) 0.8);
+		
+	
+		anal.maj(this.f.getFreqBrute(), f.getSampleRate());
+		float[] freq = CalculFourier.CutScaling(Lissage.Lissercubique(this.anal.getFFTepure()), (float) 0.8);
+		this.freqcentre = freq;
+		this.freqdroite = freq;
+		this.freqgauche = freq;
+		
+		
+	}
+
+	
+
+	
+
+}
