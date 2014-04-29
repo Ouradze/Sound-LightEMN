6ae1b0e9391f731f1b40eef0e4cb71bd0425d8f2
Willcani <facchiniguillaume@gmail.com>
2014-04-29 08:46:37 +0200

    nouveau fichier: bin/utils/Equalizer.class 	nouveau fichier: bin/utils/FileFreq.class 	nouveau fichier: bin/utils/MatriceFreq.class 	nouveau fichier: bin/utils/MatricePositions.class 	nouveau fichier: bin/utils/Point.class 	nouveau fichier: src/utils/Equalizer.java 	nouveau fichier: src/utils/FileFreq.java 	nouveau fichier: src/utils/MatriceFreq.java 	nouveau fichier: src/utils/MatricePositions.java 	nouveau fichier: src/utils/Point.java

---
+++ src/utils/MatriceFreq.java
@@ -0,0 +1,61 @@
+package utils;
+
+import sound.CalculFourier;
+
+public class MatriceFreq {
+
+	public float[] freq;
+	public int n;
+	public float[][] matrice;
+
+	public MatriceFreq(float[] f, int i) {
+		this.freq = CalculFourier.regrouper(f, i);
+		this.n = i;
+		this.construireMatriceCarresConcentriques();
+	}
+
+	public void construireMatriceCarresConcentriques() {
+		this.matrice = new float[n][n];
+		for (int i = 0; i < n; i++) {
+			for (int j = i; j < n - i; j++) {
+
+				float f = this.freq[i];
+				matrice[i][j] = f;
+				matrice[j][i] = f;
+				matrice[n - i - 1][n - j - 1] = f;
+				matrice[n - j - 1][n - i - 1] = f;
+			}
+		}
+
+	}
+
+	public void construireMatriceNulle() {
+		this.matrice = new float[n][n];
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+
+				matrice[i][j] = 0;
+
+
+			}
+		}
+	}
+
+	public void afficher() {
+
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				System.out.print(" " + this.matrice[i][j]);
+			}
+			System.out.println("");
+		}
+
+	}
+
+	public float get(int i, int j) {
+
+		return this.matrice[i][j];
+	}
+
+}
