6ff4fe29cec9edff99cb5ca23bb307516bc7a860
Willcani <facchiniguillaume@gmail.com>
2014-05-13 10:13:40 +0200

    Signed-off-by: Willcani <facchiniguillaume@gmail.com>

--- src/design/Cercles.java
+++ src/design/Cercles.java
@@ -15,30 +15,33 @@ public class Cercles extends Design {
 	@Override
 	public void display() {
 		parent.pushMatrix();
-		
-	
-		
+
 		parent.translate(this.positionX, this.positionY, 200);
-		
-		
-		
-		parent.beginShape();
-		parent.noFill();
-		parent.stroke(255);
-		for (int k = 0; k < 100; k++) {
-			double R = 10 + this.freqcentre[5] * 4;
-			float alpha = (float) (k * 2 * Math.PI) / 100;
-			parent.vertex((float) R * PApplet.cos(alpha),
-					(float) R * PApplet.sin(alpha));
+
+	
+		for (int j = 0; j < this.freqcentre.length / 2; j++) {
+			double R;
+			if (this.freqcentre[j] < 100) {
+				R = 10 * j + this.freqcentre[j]/5;
+			} else {
+				R = 10 * (j + 1);
+			}
+			parent.beginShape();
+			parent.noFill();
+			parent.stroke(255);
 			
-			
-			
+			for (int k = 0; k <= 100; k++) {
+
+				float alpha = (float) (k * 2 * Math.PI) / 100;
+				parent.vertex((float) R * PApplet.cos(alpha), (float) R
+						* PApplet.sin(alpha));
+
+			}
+			parent.endShape(PApplet.CLOSE);
 		}
-		parent.endShape(PApplet.CLOSE);
+		
 		parent.popMatrix();
 	}
-	
-	
 
 	@Override
 	public void scale() {
